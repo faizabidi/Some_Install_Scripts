6d67cf890aaf8e1f736df752fd814a21fe61c244
faizabidi <fabidi89@vt.edu>
2016-09-18 14:47:54 -0400

    Some edits.

--- create_vm2.py
+++ create_vm2.py
@@ -2,6 +2,20 @@
 
 import subprocess
 
+<<<<<<< HEAD
+retcode = subprocess.call("virt-install "
+						  "--connect qemu:///system "
+						  "-n cloudera-vm "
+						  "-r  8192 "
+						  "--vcpus=4 "
+						  "--disk path= "
+						  "/home/faiz89/Images/cloudera-quickstart-vm-5.3.0"
+						  "-0-kvm.qcow2, device=disk, bus=virtio,format=qcow2 "
+						  "--ram 8192 "
+						  "--vnc "
+						  "--noautoconsole "
+						  "--import", shell=True)
+=======
 sudo virt-install --connect qemu:///system -n vm10 -r 512 --vcpus=2 -f ~/vm10.qcow2 -s 12 -c /dev/cdrom --vnc --noautoconsole --os-type linux --accelerate --network=bridge:br0 --hvm
 
 retcode = subprocess.call("virt-install \
@@ -13,6 +27,7 @@ retcode = subprocess.call("virt-install \
 				--network network=default,model=virtio \
 				--graphics vnc,listen=0.0.0.0 \
 				--noautoconsole -v", shell=True)
+>>>>>>> 4f5bf8ac6b805124a3cceb5fb75e95268076aa75
 
 if retcode == 0:
 	print("Successfully created a VM :)")
