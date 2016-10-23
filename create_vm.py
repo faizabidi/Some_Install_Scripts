e49cabf84063346323733ec00ef1332f9ee5d22f
Faiz <fabidi89@vt.edu>
2016-10-23 12:34:34 -0400

    Minior edits.

--- create_vm.py
+++ create_vm.py
@@ -4,7 +4,7 @@ import subprocess
 
 retcode = subprocess.call("virt-install \
 				-n ubuntu1404 \
-				-r 2048 \
+				-r 8192 \
 				--disk path=/var/lib/libvirt/images/ubuntu1404.img,bus=virtio,size=8 \
 				-c /home/faiz89/Downloads/ubuntu-14.04.5-server-amd64.iso \
 				--network network=default,model=virtio \
@@ -13,6 +13,7 @@ retcode = subprocess.call("virt-install \
 
 if retcode == 0:
 	print("Successfully created a VM :)")
+    print("Type 'vncviewer localhost:0' to connect to the VM, and finish installtion.")
 else:
 	print("VM creation failed.") 			
 
--- create_vm2.py
+++ create_vm2.py
@@ -3,18 +3,15 @@
 import subprocess
 
 retcode = subprocess.call("virt-install "
-						  "--connect qemu:///system "
-						  "-n cloudera-vm "
-						  "-r  8192 "
-						  "--vcpus=4 "
-						  "--disk path= "
-						  "/home/faiz89/Images/cloudera-quickstart-vm-5.3.0"
-						  "-0-kvm\cloudera-quickstart-vm-5.3.0"
-						  "-0-kvm.qcow2, device=disk, bus=virtio,format=qcow2 "
-						  "--ram 8192 "
-						  "--vnc "
-						  "--noautoconsole "
-						  "--import", shell=True)
+    "--connect qemu:///system "
+	"-n cloudera-vm "
+    "-r  8192 "
+	"--vcpus=4 "
+	"--disk path=/home/faiz89/Images/cloudera-quickstart-vm-5.3.0-0-kvm/cloudera-quickstart-vm-5.3.0-0-kvm.qcow2,device=disk,bus=virtio,format=qcow2 "
+	"--ram 8192 "
+	"--vnc "
+	"--noautoconsole "
+	"--import", shell=True)
 
 if retcode == 0:
 	print("Successfully created a VM :)")
